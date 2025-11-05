45eb09ac0367adebc742591c090880b6f84ac905
ozataomer91-dotcom <ozataomer91@gmail.com>
2025-11-05 18:09:28 +0300

    Update signup.js

--- signup.js
+++ signup.js
@@ -19,7 +19,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAnMzCWonT_zLi0EnChIDYANBhDiiwmur4",
   authDomain: "self-training-128b5.firebaseapp.com",
   projectId: "self-training-128b5",
-  storageBucket: "self-training-128b5.firebasestorage.app",
+  storageBucket: "http://self-training-128b5.appspot.com",
   messagingSenderId: "61732879565",
   appId: "1:61732879565:web:5a446fb76fa88f1103bd84"
 };
