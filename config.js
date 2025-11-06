be00e86c7191de32cada08a36e2384a72671c082
ozataomer91-dotcom <ozataomer91@gmail.com>
2025-11-06 20:47:48 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-// config.js
-export const firebaseConfig = {
-  apiKey: "AIz...SENİN-ANAHTARIN...",
+// Firebase kimlik bilgilerin (compat ile)
+window.firebaseConfig = {
+  apiKey: "AIzaSyAnMzcWonT_zL0EhcNlDIYANBhDiivmur4",
   authDomain: "self-training-128b5.firebaseapp.com",
   projectId: "self-training-128b5",
-  storageBucket: "self-training-128b5.appspot.com",
+  storageBucket: "self-training-128b5.firebasestorage.app",
   messagingSenderId: "61732879565",
-  appId: "1:61732879565:web:5a446fb76a88f1103bd084"
+  appId: "1:61732879565:web:5a446fb76a88f1103bd84",
 };
