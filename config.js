df8e933c60b3bbdce29db183b5624a5a4cc5a5cc
ozataomer91-dotcom <ozataomer91@gmail.com>
2025-11-06 19:47:40 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 // config.js
 export const firebaseConfig = {
-  apiKey: "AIzaSyAnMzcWonT_zLioEhcNlDYANBhDiivmur4",
+  apiKey: "AIz...SENİN-ANAHTARIN...",
   authDomain: "self-training-128b5.firebaseapp.com",
   projectId: "self-training-128b5",
   storageBucket: "self-training-128b5.appspot.com",
   messagingSenderId: "61732879565",
-  appId: "1:61732879565:web:5a446fb76a88f1103bd8d4",
+  appId: "1:61732879565:web:5a446fb76a88f1103bd084"
 };
