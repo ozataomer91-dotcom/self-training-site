740a13c51f258c819425af6eef9974fc2e4ef6da
ozataomer91-dotcom <ozataomer91@gmail.com>
2025-11-09 11:51:52 +0300

    Update firebaseConfig in config.js
    Removed old comments and added a placeholder for apiKey.

--- config.js
+++ config.js
@@ -1,14 +1,9 @@
-// config.js  (yalnızca bu dosyada, tek kere)
-console.log("CONFIG OK self-training-128b5");
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAmZcW0NT_zL10EChcIDYABNdD1iwmur4",
+// DOLDURMAN GEREKEN TEK DOSYA: config.js
+// Firebase konsolundan aldığın nesneyi AYNEN buraya yapıştır.
+// ÖRNEK GÖRÜNÜM (sen kendi değerlerinle dolduracaksın):
+window.firebaseConfig = {
+  apiKey: "AIz...<seninki>",
   authDomain: "self-training-128b5.firebaseapp.com",
   projectId: "self-training-128b5",
-  storageBucket: "self-training-128b5.appspot.com",
-  messagingSenderId: "61732879565",
-  appId: "1:61732879565:web:5a4b6fb76fa06f11830d84"
+  appId: "1:6173...<seninki>"
 };
-
-// Diğer dosyalar buradan okusun diye global’e koyuyorum
-window.SELF_TRAINING = { firebaseConfig };
