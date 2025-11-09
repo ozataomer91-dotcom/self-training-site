18ce097e28c4d8862e8e927bc4bbf53168e3a969
ozataomer91-dotcom <ozataomer91@gmail.com>
2025-11-09 11:56:41 +0300

    Update Firebase configuration in config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 // Firebase konsolundan aldığın nesneyi AYNEN buraya yapıştır.
 // ÖRNEK GÖRÜNÜM (sen kendi değerlerinle dolduracaksın):
 window.firebaseConfig = {
-  apiKey: "AIz...<seninki>",
+  apiKey: "AIzasyAmZcW0NT_zL10EChcIDYABNdD1iwmur4",
   authDomain: "self-training-128b5.firebaseapp.com",
   projectId: "self-training-128b5",
-  appId: "1:6173...<seninki>"
+  appId: "1:61732879565:web:5a4b6fb76fa06f11830d84"
 };
