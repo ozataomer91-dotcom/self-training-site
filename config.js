0b7f1b42ee296045b4b96b2b8983dab94a9a4c4b
ozataomer91-dotcom <ozataomer91@gmail.com>
2025-11-09 15:04:35 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
-// self-training-site/config.js
-window.ST_CONF = {
-  // Firebase projendeki “SDK kurulum ve yapılandırma” bloğunu BURAYA yapıştırıyorsun:
-  firebaseConfig: {
-    apiKey: "AIzA...seninki...",
-    authDomain: "self-training-128b5.firebaseapp.com",
-    projectId: "self-training-128b5",
-    storageBucket: "self-training-128b5.appspot.com",
-    messagingSenderId: "61732879565",
-    appId: "1:61732879565:web:5a4b...108bd84"
-  },
-
-  // Gerekirse yönlendirme sonrası açılacak sayfa:
-  afterLoginUrl: "/self-training-site/dashboard.html"
+// yapilandirma.js
+const firebaseConfig = {
+  apiKey: "AIzaSyAnMzCWonT_zLi0EnChIDYANBhDiiwmur4",
+  authDomain: "self-training-128b5.firebaseapp.com",
+  projectId: "self-training-128b5",
+  storageBucket: "self-training-128b5.firebasestorage.app",
+  messagingSenderId: "61732879565",
+  appId: "1:61732879565:web:5a446fb76fa88f1103bd84"
 };
+
+// Firebase (compat) başlat
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+window.auth = firebase.auth();
