340fcf552dc9c0b4238ee28d788b25909f197746
ozataomer91-dotcom <ozataomer91@gmail.com>
2025-11-09 11:25:33 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,7 @@
-// Firebase config şablonu
-// Kendi projenizdeki değerleri buraya yapıştırın.
+// config.js
 window.firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "000000000000",
-  appId: "1:000000000000:web:XXXXXXXXXXXX"
+  apiKey: "",          // <- boş bırakınca DEMO açılır
+  authDomain: "",
+  projectId: "",
 };
-console.log("CONFIG OK");
+console.log("CONFIG OK self-training");
