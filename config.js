62794a3cbf58f79827b265681367f1822b2b5adc
ozataomer91-dotcom <ozataomer91@gmail.com>
2025-11-15 18:00:14 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@
   3) Enable Authentication -> Sign-in method -> Email/Password = ENABLED.
 */
 window.firebaseConfig = {
-  apiKey: "AIzaSyAnMzCWonT_zLi0EnChIDYAvNB0hiwmuz4",
+  apiKey: "AIzaSyAnMzCWonT_zLi0eCnChIDYANBhDiiwmur4",
   authDomain: "self-training-128b5.firebaseapp.com",
   projectId: "self-training-128b5",
   storageBucket: "self-training-128b5.firebasestorage.app",
-  messagingSenderId: "61732879565",
-  appId: "1:61732879565:web:5a446fb76fa88f1103bd84"
+  messagingSenderId: "617328795565",
+  appId: "1:617328795565:web:5a446fb76fa88f1103bd84"
 };
